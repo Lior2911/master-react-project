69d0d53460d257647180511a264005de1a429aa9
lior2911 <aytegev1@gmail.com>
2022-09-20 13:05:49 +0300

    first_commit 20/9

---
+++ src/components/features/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import "./Home.css";
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Home</h1>
+      <div>
+        this is a basic react tamplate project. <br />
+        this app is for copy only.
+      </div>
+    </div>
+  );
+};
+
+export default Home;
