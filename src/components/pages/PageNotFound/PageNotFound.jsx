a87e3d9dfdb8376c1e82d7276c61c31cf13822e4
lior2911 <aytegev1@gmail.com>
2022-09-21 21:33:15 +0300

    sec commit

--- src/components/pages/PageNotFound/PageNotFound.jsx
+++ src/components/pages/PageNotFound/PageNotFound.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function PageNotFound() {
   return (
     <div className="page-not-found">
-      <h1>Page Not Found</h1>
+      <h1>Page Not Found.</h1>
     </div>
   );
 };
